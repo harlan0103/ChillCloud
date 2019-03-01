a77bd22088ca4ce848b397f6eb9f7789556c628e
Haoran Liang <harlan0103@gmail.com>
2019-03-01 00:04:22 -0600

    add two components

--- src/App.js
+++ src/App.js
@@ -2,17 +2,51 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Import needed component
+import LoginForm from './Component/LoginForm';
+import MainPanel from './Component/MainPanel';
+
 class App extends Component {
+
+  // This is the constructor for App
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      login: true
+    }
+
+    // We bind the switch button and the App component
+    this.switchOnClick = this.switchOnClick.bind(this);
+  }
+
+  // We have a switch button to switch content
+  // By click this button will change the this.state.login value
+  switchOnClick() {
+    this.setState({
+      login: !this.state.login
+    })
+  }
+ 
   render() {
+    // Create a variable named content
+    var content;
+
+    if (this.state.login == true) {
+      content = <MainPanel title="Main Panel"/>;
+    }
+    else {
+      content = <LoginForm />;
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>"Hello word"</p>
-        </header>
+      <div>
+        <button onClick={this.switchOnClick}>switch</button>
+        {content}
       </div>
     );
   }
+
 }
 
 export default App;
---
+++ src/Component/LoginForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './LoginForm.css';
+
+class LoginForm extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Login</h3>
+            </div>
+        );
+    }
+}
+
+// export default means component is used externaly
+export default LoginForm;
